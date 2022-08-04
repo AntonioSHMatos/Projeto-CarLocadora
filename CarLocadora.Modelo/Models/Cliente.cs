e212d940e11800c0c40bd8d8a4a04b1879090e1e
User <User@DESKTOP-J3CC1OJ>
2022-08-04 12:05:42 -0300

    git push

--- CarLocadora.Modelo/Models/Cliente.cs
+++ CarLocadora.Modelo/Models/Cliente.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarLocadora.Models.Models
 {
-    public class ClienteModel : EnderecoModel
+    public class Cliente : EnderecoModel
     {
 
         [Key]
